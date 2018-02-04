9f52e843999cf9a83caf36afe2eb7b32f5810bf1
Michael Paccione <mpaccione1991@gmail.com>
2018-02-03 16:53:58 -0800

    added login page

--- pawsitive/routes/index.js
+++ pawsitive/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Walk with Me' });
 });
 
 module.exports = router;
