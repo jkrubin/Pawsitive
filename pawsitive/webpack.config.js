650be75d89f66a77b3287d638738f91735ff78ef
Michael Paccione <mpaccione1991@gmail.com>
2018-02-14 23:27:24 -0800

    Working on Dev Environment and Build Tools

--- pawsitive/webpack.config.js
+++ pawsitive/webpack.config.js
@@ -1,7 +1,37 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var DIST_DIR   = path.join(__dirname, "dist"),
+    CLIENT_DIR = path.join(__dirname, "src");
+
 module.exports = {
-  entry: "./app.js",
-  output: {
-    filename: "bundle.js"
-  },
-  watch: true
-}
+	context: CLIENT_DIR,
+	entry: "./src/app.js",
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: "bundle.js"
+	},
+	resolve: {
+		extensions: ['.js', '.vue', '.json', 'scss'],
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js',
+			'@': path.resolve('src'),
+			styles: path.resolve('src/assets/scss')
+		}
+	},
+	module: {
+		rules: [
+				{
+				  test: /\.scss$/,
+				  use: ExtractTextPlugin.extract({
+				    fallback: 'style-loader',
+				    use: ['css-loader', 'sass-loader']
+				  })
+				}
+			]
+	},
+	plugins: [
+    	new ExtractTextPlugin('style.css')
+  	],
+	watch: true
+};
